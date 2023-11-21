c23af4ac0bc77855b615b4ac5748fd1aa49959c7
Yonggil <gtmddls41@gmail.com>
2023-11-21 09:00:38 +0900

    Init

---
+++ EFGetStarted/DataModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace EFGetStarted
+{
+    // 클래스 이름 = 테이블 이름 = Item	
+    [Table("Item")]
+    public class Item
+    {
+        // 이름Id -> PK
+        public int ItemId { get; set; }
+        public int TemplateId { get; set; } // 101 = 집행검
+        public DateTime CreateDate { get; set; }
+
+        // 다른 클래스 참조 -> FK (Navigational Property)
+        public int OwnerId { get; set; }
+        public Player Owner { get; set; }
+    }
+
+    // 클래스 이름 = 테이블 이름 = Player	
+    public class Player
+    {
+        // 이름Id -> PK
+        public int PlayerId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ EFGetStarted/Program.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+
+namespace EFGetStarted
+{
+    internal class Program
+    {
+
+        static void InitializeDB(bool forceReset = false)
+        {
+            using (AppDbContext db = new AppDbContext())
+            {
+                if (!forceReset && (db.GetService<IDatabaseCreator>() as RelationalDatabaseCreator).Exists())
+                    return;
+
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+
+                //CreateTestData(db);
+                Console.WriteLine("DB Initialized");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            InitializeDB(forceReset: true);
+
+            //// CRUD (Create-Read-Update-Delete)
+            //Console.WriteLine("명령어를 입력하세요");
+            //Console.WriteLine("[0] Force Reset");
+            //Console.WriteLine("[1] ReadAll");
+            //Console.WriteLine("[2] UpdateDate");
+            //Console.WriteLine("[3] DeleteItem");
+
+            //while (true)
+            //{
+            //    Console.Write("> ");
+            //    string command = Console.ReadLine();
+            //    switch (command)
+            //    {
+            //        case "0":
+            //            DbCommands.InitializeDB(forceReset: true);
+            //            break;
+            //        case "1":
+            //            DbCommands.ReadAll();
+            //            break;
+            //        case "2":
+            //            DbCommands.UpdateDate();
+            //            break;
+            //        case "3":
+            //            DbCommands.DeleteItem();
+            //            break;
+            //    }
+            //}
+        }
+    }
+}
